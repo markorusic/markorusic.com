4955406638c5579ad16e9081f770deabcf63b988
Marko Rusic <marko.rusic@novafabrika.rs>
2022-01-19 03:59:43 +0100

    fix cache-factory snippet & timeline data

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -47,57 +47,116 @@ export default function Timeline() {
       <Divider />
       <Year>2021</Year>
       <ul>
-        <Step title="Something cool">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
-          similique labore incidunt dolore, omnis architecto porro illum
-          aspernatur molestiae beatae laborum ea reiciendis blanditiis! Dicta
-          cumque quisquam dolore necessitatibus consequatur.
-        </Step>
-        <Step title="Something cool">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero,
-          aliquam! Facilis mollitia ipsam officiis sapiente obcaecati!
-          Asperiores minima debitis laboriosam? Nesciunt veritatis expedita
-          reiciendis aut nisi in architecto illum officia!
-        </Step>
+        <Step title="Droped Out of Faculty"></Step>
+        <Step title="Devoted Myself to Programming"></Step>
+        <Step title="Started New Studies"></Step>
+        <Step title="Found First Dev Job"></Step>
+      </ul>
+      <Divider />
+      <Year>2020</Year>
+      <ul>
+        <Step title="Got Into React Query"></Step>
       </ul>
       <Divider />
       <Year>2019</Year>
       <ul>
-        <Step title="Something cool">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
-          similique labore incidunt dolore, omnis architecto porro illum
-          aspernatur molestiae beatae laborum ea reiciendis blanditiis! Dicta
-          cumque quisquam dolore necessitatibus consequatur.
+        <Step title="Got Into Testing"></Step>
+        <Step title="Leared About Clinet Side Caching Techniques"></Step>
+        <Step title="Built React Native App"></Step>
+        <Step title="Leared About Monorepos"></Step>
+        <Step title="Promoted to Team Leader Position"></Step>
+      </ul>
+      <Divider />
+      <Year>2018</Year>
+      <ul>
+        <Step title="Got Into Telco Industry"></Step>
+        <Step title="Side Projects"></Step>
+        <Step title="Hackathons"></Step>
+        <Step title="Built my first backend app with Java/Spring Boot">
+          It was backend for CMS that
         </Step>
-        <Step title="Something cool">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero,
-          aliquam! Facilis mollitia ipsam officiis sapiente obcaecati!
-          Asperiores minima debitis laboriosam? Nesciunt veritatis expedita
-          reiciendis aut nisi in architecto illum officia!
+        <Step title="Built Web Apps With Vue.js"></Step>
+        <Step title="Experimented With Tech Stacks"></Step>
+        <Step title="Got Into React/React Native"></Step>
+        <Step title="Architected Huge Web App"></Step>
+      </ul>
+      <Divider />
+      <Year>2017</Year>
+      <ul>
+        <Step title="Droped Out of Faculty">
+          Decided to give up on math, and dedicate myself to something else...
         </Step>
-        <Step title="Something cool">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
-          similique labore incidunt dolore, omnis architecto porro illum
-          aspernatur molestiae beatae laborum ea reiciendis blanditiis! Dicta
-          cumque quisquam dolore necessitatibus consequatur.
+        <Step title="Devoted Myself to Programming">
+          This was a huge turing point in my life and career. I started studing
+          programming by myself and everything instantly clicked. I relized that
+          this is what I wanted to do in my life.
         </Step>
-        <Step title="Something cool">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero,
-          aliquam! Facilis mollitia ipsam officiis sapiente obcaecati!
-          Asperiores minima debitis laboriosam? Nesciunt veritatis expedita
-          reiciendis aut nisi in architecto illum officia!
+        <Step title="Got Paid to Build Website">
+          This was the first time someone paid me to write code, and it felt
+          amazing. It was small ecommerce website, writen in vanilla PHP and
+          jQuery. It was a huge thing for me, because I learned about relational
+          databases, server-side programming, REST APIs, AJAX, CSS animations,
+          deploying to production environment, and so many more things. But the
+          most important thing I learned was how to think like a programmer, how
+          to solve real world problems with tools like programing languages.
         </Step>
-        <Step title="Something cool">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
-          similique labore incidunt dolore, omnis architecto porro illum
-          aspernatur molestiae beatae laborum ea reiciendis blanditiis! Dicta
-          cumque quisquam dolore necessitatibus consequatur.
+        <Step title="Started New Studies in Field of Informational Technologies">
+          Decided to give another try to college, only this time I knew what I
+          actually like and want. I enrolled in{' '}
+          <a
+            className="text-blue-500 underline"
+            href="https://ict.edu.rs"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            ICT College of Vocational Studies.
+          </a>{' '}
+          They have very good practial study programs, that try to emulate real
+          world environment for IT industry and that way prepare students for
+          their first jobs.
         </Step>
-        <Step title="Something cool">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero,
-          aliquam! Facilis mollitia ipsam officiis sapiente obcaecati!
-          Asperiores minima debitis laboriosam? Nesciunt veritatis expedita
-          reiciendis aut nisi in architecto illum officia!
+        <Step title="Started First Dev Job">
+          After a few unsuccessful interviews, finally got my first dev job at
+          NetCast. I was very happy, enthusiastic and ready to grind and learn
+          as much as I can.
+        </Step>
+        <Step title="The First Project That Attracted a Lot of Attention">
+          <a
+            className="text-blue-500 underline"
+            href="https://www.slikesrbije.rs"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Slike Srbije
+          </a>{' '}
+          was my first project and it quickly got a lot of attention. It was
+          basically a website where photographers competed for the most
+          beautiful picture of Serbia. It got more than 200k unique visits over
+          3 months, had around 40k registrated users, and more than 500 active
+          participants. It was built with MySQL, Laravel and Vue.
+        </Step>
+      </ul>
+      <Divider />
+      <Year>2016</Year>
+      <ul>
+        <Step title="Graduated High School">
+          There I had my first real introduction to programming with C, C#,
+          HTML, CSS, JavaScript, SQL, etc...
+        </Step>
+        <Step title="Started Faculty of Mathematics, University of Belgrade 🤦">
+          Basically I got in here because I was always fairly good at math and I
+          really tought I liked it, but it turned out that I was very wrong.
+        </Step>
+        <Step title="Wanted To Dropout of Faculty">
+          I am thankful to "Mathematical Analysis", because it made me realized
+          that I was not interested in that level math.
+        </Step>
+      </ul>
+      <Divider />
+      <Year>2007</Year>
+      <ul>
+        <Step title="First Computer 🕹">
+          Warcraft 3 and Dota were so much fun.
         </Step>
       </ul>
       <Divider />
