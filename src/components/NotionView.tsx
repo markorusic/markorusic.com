e6fb147a97455133b257380069e0234588ebbbb1
Marko Rusic <markorusic98@gmail.com>
2023-11-01 01:17:19 +0100

    --

--- src/components/NotionView.tsx
+++ src/components/NotionView.tsx
@@ -1,18 +1,34 @@
-import { FC, Fragment, ReactNode, createElement } from 'react';
+import {
+  FC,
+  Fragment,
+  ReactNode,
+  createElement,
+  useEffect,
+  useState
+} from 'react';
 import clsx from 'clsx';
+import { copyText, slugify } from '@/lib/utils';
 import {
   BlockObjectResponse,
   BulletedListItemBlockObjectResponse,
+  CalloutBlockObjectResponse,
   CodeBlockObjectResponse,
+  DividerBlockObjectResponse,
   Heading1BlockObjectResponse,
   Heading2BlockObjectResponse,
   Heading3BlockObjectResponse,
   ImageBlockObjectResponse,
   ParagraphBlockObjectResponse,
+  QuoteBlockObjectResponse,
   RichTextItemResponse
 } from '@notionhq/client/build/src/api-endpoints';
-import SyntaxHighlighter from 'react-syntax-highlighter';
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { nightOwl as syntaxHighlighterStyle } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+import js from 'react-syntax-highlighter/dist/cjs/languages/hljs/javascript';
+import ts from 'react-syntax-highlighter/dist/cjs/languages/hljs/typescript';
+
+SyntaxHighlighter.registerLanguage('js', js);
+SyntaxHighlighter.registerLanguage('ts', ts);
 
 type BlockComponent<T extends BlockObjectResponse = BlockObjectResponse> = FC<{
   block: T;
@@ -38,10 +54,6 @@ export const NotionView: FC<NotionViewProps> = ({ blocks = [] }) => {
   );
 };
 
-const Todo: BlockComponent = ({ block }) => {
-  return <div>{block.id}</div>;
-};
-
 const Heading1: BlockComponent<Heading1BlockObjectResponse> = ({ block }) => {
   return (
     <h1>
@@ -50,15 +62,19 @@ const Heading1: BlockComponent<Heading1BlockObjectResponse> = ({ block }) => {
   );
 };
 const Heading2: BlockComponent<Heading2BlockObjectResponse> = ({ block }) => {
+  const id = slugify(richText(block.heading_2.rich_text));
   return (
-    <h2>
+    <h2 id={id}>
+      <a className="anchor" href={`#${id}`} />
       <RichText text={block.heading_2.rich_text} />
     </h2>
   );
 };
 const Heading3: BlockComponent<Heading3BlockObjectResponse> = ({ block }) => {
+  const id = slugify(richText(block.heading_3.rich_text));
   return (
-    <h3>
+    <h3 id={id}>
+      <a className="anchor" href={`#${id}`} />
       <RichText text={block.heading_3.rich_text} />
     </h3>
   );
@@ -88,14 +104,36 @@ const Image: BlockComponent<ImageBlockObjectResponse> = ({ block }) => {
   return <img src={src} alt={richText(block.image.caption)} />;
 };
 const Code: BlockComponent<CodeBlockObjectResponse> = ({ block }) => {
+  const text = richText(block.code.rich_text);
   return (
-    <SyntaxHighlighter
-      language={block.code.language}
-      style={syntaxHighlighterStyle}
-      wrapLongLines
-    >
-      {richText(block.code.rich_text)}
-    </SyntaxHighlighter>
+    <div className="relative">
+      <CopyButton className="absolute text-white top-3 right-3" text={text} />
+      <SyntaxHighlighter
+        language={block.code.language}
+        style={syntaxHighlighterStyle}
+        customStyle={{ padding: 16 }}
+        wrapLongLines
+      >
+        {text}
+      </SyntaxHighlighter>
+    </div>
+  );
+};
+const Divider: BlockComponent<DividerBlockObjectResponse> = () => {
+  return <div className="w-full h-[1px] bg-gray-600 my-4" />;
+};
+const Quote: BlockComponent<QuoteBlockObjectResponse> = ({ block }) => {
+  return (
+    <div className="my-5 py-1 px-4 border-l-2 border-black dark:border-white">
+      <RichText text={block.quote.rich_text} />
+    </div>
+  );
+};
+const Callout: BlockComponent<CalloutBlockObjectResponse> = ({ block }) => {
+  return (
+    <div className="bg-gray-200 text-black dark:text-white dark:bg-gray-800 p-4 rounded my-5">
+      <RichText text={block.callout.rich_text} />
+    </div>
   );
 };
 
@@ -107,7 +145,9 @@ const blockComponentMap: BlockComponentMap = {
   bulleted_list_item: BulletedListItem,
   image: Image,
   code: Code,
-  divider: Todo
+  divider: Divider,
+  quote: Quote,
+  callout: Callout
 };
 
 const richText = (text: RichTextItemResponse[]) =>
@@ -150,7 +190,7 @@ const RichText: FC<{ text: RichTextItemResponse[] }> = ({ text }) => {
 const colorMap: Partial<
   Record<RichTextItemResponse['annotations']['color'], string>
 > = {
-  gray: 'text-gray-300'
+  gray: 'text-gray-600 dark:text-gray-300'
 };
 
 const getTextStyle = (annotation: RichTextItemResponse['annotations']) =>
@@ -159,6 +199,57 @@ const getTextStyle = (annotation: RichTextItemResponse['annotations']) =>
     italic: annotation.italic,
     underline: annotation.underline,
     'line-through': annotation.strikethrough,
-    'bg-gray-200 text-black dark:text-white dark:bg-gray-600 font-medium px-1 py-0.5 rounded':
+    'bg-gray-200 text-black dark:text-white dark:bg-gray-800 font-medium px-1 py-0.5 rounded':
       annotation.code
   });
+
+type CopyButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  text: string;
+};
+
+type CopyState = 'idle' | 'success' | 'error';
+
+const CopyButton: FC<CopyButtonProps> = ({ text, ...props }) => {
+  const [state, setState] = useState<CopyState>('error');
+
+  useEffect(() => {
+    if (state !== 'idle') {
+      const timeout = setTimeout(() => setState('idle'), 2500);
+      return () => clearTimeout(timeout);
+    }
+  }, [state]);
+
+  return (
+    <button
+      {...props}
+      onClick={(event) => {
+        event.preventDefault();
+        void copyText(text)
+          .then(() => setState('success'))
+          .catch(() => setState('error'));
+      }}
+    >
+      <svg
+        className="w-5 h-5"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <rect width="14" height="14" x="8" y="8" rx="2" ry="2" />
+        <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" />
+      </svg>
+
+      {state !== 'idle' && (
+        <span className="absolute -bottom-2 -translate-x-1/2 translate-y-full px-1 py-0.5 text-xs bg-gray-400 rounded">
+          {state === 'success' ? 'Copied!' : 'Failed!'}
+        </span>
+      )}
+    </button>
+  );
+};
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -18,3 +18,51 @@ export const pick = <Obj, Keys extends keyof Obj>(
     return acc;
   }, {} as any);
 };
+
+export const copyText = (text: string) => {
+  if (navigator.clipboard) {
+    return navigator.clipboard.writeText(text);
+  }
+  let succees = false;
+  const textArea = document.createElement('textarea');
+  textArea.value = text;
+
+  textArea.style.top = '0';
+  textArea.style.left = '0';
+  textArea.style.position = 'fixed';
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    succees = document.execCommand('copy');
+  } catch {
+    succees = false;
+  } finally {
+    document.body.removeChild(textArea);
+  }
+
+  if (succees) {
+    return Promise.resolve('Copied');
+  }
+  return Promise.reject('Copy failed');
+};
+
+export const slugify = (input: string) => {
+  let str = input.replace(/^\s+|\s+$/g, '');
+  str = str.toLowerCase();
+  const from = 'àáäâèéëêìíïîòóöôùúüûñç·/_,:;';
+  const to = 'aaaaeeeeiiiioooouuuunc------';
+
+  for (let i = 0, l = from.length; i < l; i++) {
+    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+  }
+
+  str = str
+    .replace(/[^a-z0-9 -]/g, '')
+    .replace(/\s+/g, '-')
+    .replace(/-+/g, '-');
+
+  return str;
+};
