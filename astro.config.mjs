43fa1879207f8cadb4801a8e2697751245f31c9a
Marko Rusic <markorusic98@gmail.com>
2024-07-09 20:01:51 +0200

    cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,8 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://markorusic-com-v2.vercel.app",
   integrations: [mdx(), sitemap(), tailwind()],
-  output: "server",
-  adapter: vercel(),
 });
