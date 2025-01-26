a37970a96881c6c9802181089906ed756a1d2f68
Marko Rusic <markorusic98@gmail.com>
2025-01-26 17:14:25 +0100

    better code highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,15 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import expressiveCode from "astro-expressive-code";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://markorusic-com-v2.vercel.app",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [
+    expressiveCode({ themes: ["vitesse-dark"] }),
+    mdx(),
+    sitemap(),
+    tailwind(),
+  ],
 });
